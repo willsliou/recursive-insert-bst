2c33482e302b3f55324635c3be8bf84a1a4ec77c
Wills Liou <willsliou@protonmail.com>
2020-11-03 16:24:46 +0000

    root

--- main.cpp
+++ main.cpp
@@ -9,16 +9,26 @@ struct Node {
 
 Node* insert(Node *p, int key) {
     Node *t;
-    // No nodes exist
+    // Node is null
     if (p == nullptr) {
         // Create a new node
         t = new Node;
         // Put in new data into node and set right/left children to NULL
         t->data = key;
         t->left = t->right = nullptr;
-        
     }
+    
+    if (key < p->data) {
+        // create a new node then assign to left child of previous node (in call stack)
+        p->left = insert(p->left, key); // p->left will give null during our recursive call, which will default to the base case and create a new node.
+    } 
+    else if (key > p->data) {
+        p->right = insert(p->right, key); 
+    }
+    // return back our address to our node
+    return p;
 }
+
 int main() {
     // Create root node
     Node *root = nullptr;
